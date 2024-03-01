3ad375d40c3e08aa477293085d4358d033530560
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2024-03-02 03:02:51 +0530

    Food Ordering Website basic layout

--- App.js
+++ App.js
@@ -1,18 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Element = () => {
-    return (<div>
-        <h1>
-            <h2>
-                <h3>
-                    Hello from React Functional Component!
-                </h3>
-            </h2>
-        </h1>
+/*
+* Header
+    - Logo
+    - Nav Items
+    - 
+* Body
+    - Search
+    - Resturant Container
+    - Resturant Card
+* Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+*/
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://cdn.logojoy.com/wp-content/uploads/2018/05/01105937/16_big6-768x591.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="Search">Search</div>
+            <div className="resturant-container">
+                // Resturant Card
+            </div>
+        </div>
+    )
+};
+
+const AppLayout = () => {
+    return(<div className="app">
+        <Header/>
     </div>)
 };
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Element/>);
+root.render(<AppLayout/>);
