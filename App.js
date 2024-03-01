7a2f7c1764798373f8c787f8cbd7c6b7a7631f6d
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2024-03-02 01:15:01 +0530

    Assignment 03-03

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = <div>
-    <h1>
-        <h2>
-            <h3>
-                Hello from React JSX!
-            </h3>
-        </h2>
-    </h1>
-</div>
+const Element = () => {
+    return (<div>
+        <h1>
+            <h2>
+                <h3>
+                    Hello from React Functional Component!
+                </h3>
+            </h2>
+        </h1>
+    </div>)
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<Element/>);
